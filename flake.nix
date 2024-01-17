36355bfea26a1587d9fbfc8b4970b0c7f2e914f2
Max Moir <max.moir@icloud.com>
2024-01-17 18:27:54 +0000

    Finally fixed

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
             inherit username; inherit hostname; inherit gitUsername;
             inherit gitEmail; inherit theLocale; inherit theTimezone;
         };
-	    modules = [ ./thethnker/configuration.nix
+	    modules = [ ./thethinker/configuration.nix
           home-manager.nixosModules.home-manager {
 	        home-manager.extraSpecialArgs = { inherit username; 
                 inherit gitUsername; inherit gitEmail; inherit inputs;

9a3e28176d6637b60cb7188dfcbe0d1a6509168b
Max Moir <max.moir@icloud.com>
2024-01-17 17:43:37 +0000

    It seems to have built successfully

--- home.nix
+++ home.nix
@@ -124,9 +124,9 @@
     enable = true;
   };
 
-  home.file.".config/nvim".source = "${flakeDir}.config/nvim";
+  home.file.".config/nvim".source = ./.config/nvim;
   home.file.".config/nvim".recursive = true;
 
-  home.file.".config/pylint".source = "${flakeDir}.config/pylint";
+  home.file.".config/pylint".source = ./.config/pylint;
   home.file.".config/pylint".recursive = true;
 }

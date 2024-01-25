7b582dcaa6abcdcfd2815cc9024df249787be5d6
Max Moir <max.moir@icloud.com>
2024-01-25 00:00:42 +0000

    feat: Moved extension configs to nix

--- config/home/dconf.nix
+++ config/home/dconf.nix
@@ -7,6 +7,55 @@ with lib.hm.gvariant;
   dconf = {
     enable = true;
     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
+    # Enabled extensions
+    settings."org/gnome/shell" = {
+      disabled-extensions = [
+        "screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com"
+        "native-window-placement@gnome-shell-extensions.gcampax.github.com"
+        "drive-menu@gnome-shell-extensions.gcampax.github.com"
+        "light-style@gnome-shell-extensions.gcampax.github.com"
+        "window-list@gnome-shell-extensions.gcampax.github.com"
+        "places-menu@gnome-shell-extensions.gcampax.github.com"
+        "apps-menu@gnome-shell-extensions.gcampax.github.com"
+        "workspace-indicator@gnome-shell-extensions.gcampax.github.com"
+      ];
+      enabled-extensions = [
+        "launch-new-instance@gnome-shell-extensions.gcampax.github.com"
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "Battery-Health-Charging@maniacx.github.com"
+        "blur-my-shell@aunetx"
+        "color-picker@tuberry"
+        "mediacontrols@cliffniff.github.com"
+        "Vitals@CoreCoding.com"
+        "windowsNavigator@gnome-shell-extensions.gcampax.github.com"
+      ];
+      favorite-apps = [
+        "firefox.desktop"
+        "thunderbird.desktop"
+        "spotify.desktop"
+        "org.gnome.Nautilus.desktop"
+        "kitty.desktop"
+        "org.prismlauncher.PrismLauncher.desktop"
+      ];
+    };
+
+    # Keybindings
+    settings."org/gnome/desktop/wm/keybindings" = {
+      panel-run-dialog = ["<Super>R"];
+      switch-applications = ["<Super>Tab"];
+      switch-applications-backward = ["<Shift><Super>Tab"];
+      switch-windows = ["<Alt>Tab"];
+      switch-windows-backward = ["<Alt><Shift>Tab"];
+      toggle-fullscreen = ["F11"];
+    };
+
+    settings."org/gnome/desktop/wm/preferences" = {
+      button-layout = "appmenu:minimize,maximize,close";
+      visual-bell = false;
+    };
+
+    # Blur My Shell
     settings."org/gnome/shell/extensions/blur-my-shell" = {
       brightness = 0.64;
       color = (mkTuple [0.356 0.054 0.397 0.233]);
@@ -30,6 +79,51 @@ with lib.hm.gvariant;
     };
     settings."org/gnome/shell/extensions/blur-my-shell/panel" = {
       override-background = true;
-    }; 
+    };
+
+    # Color Picker
+    settings."org/gnome/shell/extensions/color-picker" = {
+      default-format = 0;
+      enable-shortcut = true;
+      enable-systray = true;
+      menu-size = 1;
+    };
+
+    # Media Controls
+    settings."org/gnome/shell/extensions/mediacontrols" = {
+      colored-player-icon = false;
+      extension-index = 0;
+      extension-position = "right";
+      hide-media-notification = true;
+      max-widget-width = 200;
+      mouse-actions = ["toggle_info" "toggle_menu" "raise" "none" "none" "none" "none" "none"];
+      scroll-track-label = true;
+      show-player-icon = false;
+      show-seek-back = false;
+      show-seek-forward = false;
+      show-seperators = false;
+      show-sources-menu = false;
+      track-label = ["track" "-" "artist"];
+    };
+
+    # Vitals
+    settings."org/gnome/shell/extensions/vitals" = {
+      alphabetize = true;
+      hide-icons = false;
+      hide-zeros = false;
+      hot-sensors = ["_processor_usage_" "_processor_frequency_" "__temperature_avg__" "_memory_usage_"];
+      menu-centered = false;
+      position-in-panel = 0;
+      show-battery = false;
+      show-memory = true;
+      update-time = 3;
+      use-higher-precision = true;
+    };
+
+    # Window List
+    settings."org/gnome/shell/extensions/window-list" = {
+      display-all-workspaces = false;
+      grouping-mode = "never";
+    };
   };
 }

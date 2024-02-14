6652075abd2e5a10763e7a82f3544cda1ef0a2ee
Max Moir <max.moir@icloud.com>
2024-02-14 23:40:27 +0000

    Revert "feat: The fuck?\n\nadded thefuck"
    This reverts commit 24ce889a40bee4e6a604e867f07b657900f230f0.

--- config/home/zsh.nix
+++ config/home/zsh.nix
@@ -6,16 +6,13 @@
   imports = [
     ./shell/eza.nix
     ./shell/fzf.nix
-    ./shell/thefuck.nix
     ./shell/zoxide.nix
   ];
- 
+  
   programs.zsh = {
     enable = true;
     dotDir = ".config/zsh";
     shellAliases = {
-      cd = "z";
-      cdi = "zi";
       cat = "bat";
       bonk = "clear";
       nixconfig = "cd ~/nixos-dotfiles/";
@@ -29,10 +26,5 @@
             ];
       theme = "robbyrussell";
     };
-  };
-
-  programs.thefuck = {
-    enable = true;
-    enableZshIntegration = true;
   };
 }

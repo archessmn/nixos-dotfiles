e0e1be194fae07867ab28795c4673977db09b0c9
Mia Moir <me@archess.mn>
2024-09-16 02:26:44 +0100

    chore: Update SSH keys with honkpad

--- config/ssh/keys.nix
+++ config/ssh/keys.nix
@@ -2,4 +2,5 @@
   adrasteia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDcmDNjKEltZB9ZTijvfnka09iTJQ5Ro7QXBKhXpC4Ey archessmn@adrasteia";
   temjin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMh2mUHL7drQsnB/3q4PDBD6XoQTB6OOr5bXXvqwlxYa archessmn@temjin";
   zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICFI0pAOSkn/deHOz31uavaCWQ770w9Bxv3MVC9Jsts3 archessmn@zenith";
+  honkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICR2wCQqZvFKmKByOGBAAjv4EdsFXvxzbVx0a15EjBpz archessmn@honkpad";
 }

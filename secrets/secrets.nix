6b8349c192a008efdd7b022c0c8b11d28685ff2f
Mia Moir <me@archess.mn>
2025-02-08 01:54:55 +0000

    Update secrets for zenith

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,12 +1,13 @@
 let
-  archessmn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDcmDNjKEltZB9ZTijvfnka09iTJQ5Ro7QXBKhXpC4Ey archessmn@adrasteia";
-  users = [ archessmn ];
+  archessmn-adrasteia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDcmDNjKEltZB9ZTijvfnka09iTJQ5Ro7QXBKhXpC4Ey archessmn@adrasteia";
+  archessmn-zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICFI0pAOSkn/deHOz31uavaCWQ770w9Bxv3MVC9Jsts3 archessmn@zenith";
+  users = [ archessmn-adrasteia archessmn-zenith ];
 
   tsuro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJW1+M6OciB/nTfRTuTUUECmRqZ+J0deoMfNCfv0pRI";
   systems = [ tsuro ];
 in
 {
-  "traefik_cloudflare.env.age".publicKeys = [ archessmn tsuro ];
-  "traefik_kanidm.env.age".publicKeys = [ archessmn tsuro ];
-  "vaultwarden.env.age".publicKeys = [ archessmn tsuro ];
+  "traefik_cloudflare.env.age".publicKeys = [ archessmn-adrasteia archessmn-zenith tsuro ];
+  "traefik_kanidm.env.age".publicKeys = [ archessmn-adrasteia archessmn-zenith tsuro ];
+  "vaultwarden.env.age".publicKeys = [ archessmn-adrasteia archessmn-zenith tsuro ];
 }

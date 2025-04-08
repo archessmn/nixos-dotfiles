69f18c44f496f6ddf7f48c65fddd0a348b39258b
Mia Moir <me@archess.mn>
2025-04-08 21:26:53 +0100

    chore(tsuro): Update uptime kuma

--- modules/archessmn/roles/uptime-kuma.nix
+++ modules/archessmn/roles/uptime-kuma.nix
@@ -29,7 +29,7 @@ in
     virtualisation.oci-containers.containers.uptime-kuma = {
       autoStart = true;
 
-      image = "louislam/uptime-kuma:1.23.15";
+      image = "louislam/uptime-kuma:1.23.16";
 
       ports = [
         "3001:3001"

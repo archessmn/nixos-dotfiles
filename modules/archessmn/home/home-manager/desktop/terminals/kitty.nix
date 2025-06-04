c8a30c8c1035a57170984f9d0dfa3dcd5318a668
Mia Moir <me@archess.mn>
2025-06-04 20:55:49 +0100

    fix: Revert name of font

--- modules/archessmn/home/home-manager/desktop/terminals/kitty.nix
+++ modules/archessmn/home/home-manager/desktop/terminals/kitty.nix
@@ -26,7 +26,7 @@ in
     programs.kitty = {
       enable = true;
       font.package = pkgs.nerd-fonts.fira-mono;
-      font.name = "FiraMono Nerd Font Mono";
+      font.name = "FiraMono Nerd Font";
       extraConfig = "linux_display_server X11";
 
       shellIntegration = {

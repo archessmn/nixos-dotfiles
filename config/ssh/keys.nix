a0df7957eb0420e787b502431b6c9fa02dd3f7d3
Mia Moir <me@archess.mn>
2025-06-17 18:07:38 +0100

    chore(ssh): Add godshawke ssh key

--- config/ssh/keys.nix
+++ config/ssh/keys.nix
@@ -1,5 +1,6 @@
 {
   adrasteia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDcmDNjKEltZB9ZTijvfnka09iTJQ5Ro7QXBKhXpC4Ey archessmn@adrasteia";
+  godshawke = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0HpThCYE2gOZvrU8sRFqeOKS4llKMzALqx5+T+zxpV archessmn@godshawke";
   honkpad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICR2wCQqZvFKmKByOGBAAjv4EdsFXvxzbVx0a15EjBpz archessmn@honkpad";
   nostromo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjm5bvRdZcofeBZ/lhke5hIKRAyejACr3tw7LQJUfl7 archessmn@nostromo";
   polaris = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ7wPQ8TlD6kG3ie6J3rI/TSnEOCSyTHObYGTaxTtnb+ archessmn@polaris";

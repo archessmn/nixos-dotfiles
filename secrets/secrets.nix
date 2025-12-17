aaabab2a7077b9c1ba40b743413584141180fc8a
Mia Moir <me@archess.mn>
2025-12-17 18:55:41 +0000

    Agenix secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,13 +1,18 @@
 let
   archessmn-adrasteia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDcmDNjKEltZB9ZTijvfnka09iTJQ5Ro7QXBKhXpC4Ey archessmn@adrasteia";
+  archessmn-helios = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAKbv0CfhD/1mE+OORtFtHcj9PA3Gal6S/+czXp82B0t archessmn@helios";
+  archessmn-temjin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMh2mUHL7drQsnB/3q4PDBD6XoQTB6OOr5bXXvqwlxYa archessmn@temjin";
   archessmn-zenith = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICFI0pAOSkn/deHOz31uavaCWQ770w9Bxv3MVC9Jsts3 archessmn@zenith";
   users = [
     archessmn-adrasteia
+    archessmn-helios
+    archessmn-temjin
     archessmn-zenith
   ];
 
+  temjin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA3TSB/Hx9BYN+UeGpVXQFtetVyuZ72MNcg0ADHGrxNP";
   tsuro = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJW1+M6OciB/nTfRTuTUUECmRqZ+J0deoMfNCfv0pRI";
-  systems = [ tsuro ];
+  systems = [ temjin tsuro ];
 in
 {
   "traefik_cloudflare.env.age".publicKeys = [
@@ -29,5 +34,17 @@ in
     archessmn-adrasteia
     archessmn-zenith
     tsuro
+  ];
+
+  "temjin_traefik_cloudflare.env.age".publicKeys = [
+    archessmn-helios
+    archessmn-temjin
+    temjin
+  ];
+
+  "temjin_traefik_kanidm.env.age".publicKeys = [
+    archessmn-helios
+    archessmn-temjin
+    temjin
   ];
 }

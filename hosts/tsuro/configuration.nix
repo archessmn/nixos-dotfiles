7b1a30d9eb06fc0cd7865eaaa9d77d1f44981e01
Mia Moir <me@archess.mn>
2025-12-17 19:36:30 +0000

    Disable resolved on tsuro

--- hosts/tsuro/configuration.nix
+++ hosts/tsuro/configuration.nix
@@ -93,8 +93,6 @@
     };
   };
 
-  services.resolved.enable = true;
-
   # Include Agenix Secrets
   age.secrets.traefik_cloudflare_env.file = ../../secrets/${hostname}_traefik_cloudflare.env.age;
   age.secrets.traefik_kanidm_env.file = ../../secrets/${hostname}_traefik_kanidm.env.age;

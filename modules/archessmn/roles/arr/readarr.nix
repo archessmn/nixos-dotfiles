44a156b166dc8d0286d57603206c084fff6d9a91
Mia Moir <me@archess.mn>
2025-12-26 21:11:56 +0000

    role readarr: Create

---
+++ modules/archessmn/roles/arr/readarr.nix
@@ -0,0 +1,48 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib;
+let
+  cfg = config.archessmn.roles.arr.readarr;
+in
+{
+  options.archessmn.roles.arr.readarr = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    virtualisation.oci-containers.backend = "docker";
+
+    virtualisation.oci-containers.containers.readarr = {
+      autoStart = true;
+
+      image = "ghcr.io/hotio/readarr:testing-0.4.18.2805";
+
+      volumes = [
+        "/opt/readarr/config:/config"
+        "/deep-storage-pool:/data"
+      ];
+
+      ports = [
+        "7878:7878"
+      ];
+
+      environment = {
+        PUID = "1000";
+        PGID = "1";
+        UMASK = "002";
+        TZ = "Etc/UTC";
+      };
+
+      labels = {
+        "traefik.enable" = "true";
+        "traefik.http.routers.readarr.rule" = "Host(`readarr.moir.xyz`)";
+      };
+    };
+  };
+}

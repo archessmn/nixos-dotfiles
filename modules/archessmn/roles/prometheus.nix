a3e3cf8ef20f3309d5814be5e57bec83acd0527d
Mia Moir <me@archess.mn>
2025-12-27 23:00:28 +0000

    role prometheus: Create

---
+++ modules/archessmn/roles/prometheus.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib;
+let
+  cfg = config.archessmn.roles.prometheus;
+in
+{
+  options.archessmn.roles.prometheus = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+
+  config = mkIf cfg.server.enable {
+    services.prometheus = {
+      enable = true;
+    };
+  };
+}

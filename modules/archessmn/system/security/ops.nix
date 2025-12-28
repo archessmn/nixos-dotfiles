536909682bb82bf733481cc6dd1ad1b0b91996fb
Mia Moir <me@archess.mn>
2025-12-28 19:00:33 +0000

    ops: Change ssh key

--- modules/archessmn/system/security/ops.nix
+++ modules/archessmn/system/security/ops.nix
@@ -29,7 +29,7 @@ in
       hashedPassword = "$y$j9T$1Knq0KkjTsgJSaXSoVib31$iQEJC4/gr1dCKz9tYGp7jyGxV9Op9gFsUJ.ZaNvVYLC";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKGslDZkmwJa0h8LC8v8A+XRRcwK3MqY72pFUQqeHAXX archessmn@ops-user"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPG1n1nIumnB5Vpc8En1z96TzwmmNStjCYwz6fBp3QRt jenkins@temjin"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPcXDExjR2UOfcmas0AM6l0c+jW/yq7ViIyPu3/nyC8l jenkins@temjin"
       ];
       packages = with pkgs; [ jdk21 ];
     };

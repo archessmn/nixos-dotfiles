414bac4a222f75c22d54afdc84ff810b3e898e36
Mia Moir <me@archess.mn>
2026-04-14 21:29:52 +0100

    role asterisk: Try hello world

--- modules/archessmn/roles/asterisk.nix
+++ modules/archessmn/roles/asterisk.nix
@@ -21,6 +21,41 @@ in
   config = mkIf cfg.enable {
     services.asterisk = {
       enable = true;
+
+      confFiles = {
+        "extensions.conf" = ''
+          [from-internal]
+          exten = 100,1,Answer()
+          same = n,Wait(1)
+          same = n,Playback(hello-world)
+          same = n,Hangup()
+        '';
+
+        "pjsip.conf" = ''
+          [transport-udp]
+          type=transport
+          protocol=udp
+          bind=0.0.0.0
+
+          [6001]
+          type=endpoint
+          context=from-internal
+          disallow=all
+          allow=ulaw
+          auth=6001
+          aors=6001
+
+          [6001]
+          type=auth
+          auth_type=userpass
+          password=unsecurepassword
+          username=6001
+
+          [6001]
+          type=aor
+          max_contacts=1
+        '';
+      };
     };
   };
 }

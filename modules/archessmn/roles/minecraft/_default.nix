4e39e54220bebfcf57f6aa319d0ddb1b4624518b
Mia Moir <me@archess.mn>
2026-06-21 21:45:24 +0100

    role minecraft: Change to 26.2

--- modules/archessmn/roles/minecraft/_default.nix
+++ modules/archessmn/roles/minecraft/_default.nix
@@ -33,7 +33,7 @@ in
           archessmn = "a2bb6157-d5e7-4d2a-9a6e-e534b0b17235";
           Empleon = "7362abdc-61bb-465a-8c4e-9744520d5bb4";
         };
-        package = pkgs.fabricServers.fabric-1_21_1;
+        package = pkgs.fabricServers.fabric-26_2;
       };
     };
   };

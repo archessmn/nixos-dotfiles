0fe234a6ab29e2dda88024f19317b519ab0e78a6
Mia Moir <me@archess.mn>
2026-06-22 16:55:16 +0100

    role frigate: Update to 0.17.1

--- modules/archessmn/roles/frigate.nix
+++ modules/archessmn/roles/frigate.nix
@@ -21,7 +21,7 @@ in
     virtualisation.oci-containers.containers.frigate = {
       autoStart = true;
 
-      image = "ghcr.io/blakeblackshear/frigate:0.16.3";
+      image = "ghcr.io/blakeblackshear/frigate:0.17.1";
 
       ports = [
         "5000:5000"

0de655f732b7e15fc31b2c9d413d9160a4917e5c
Mia Moir <me@archess.mn>
2026-07-04 12:31:15 +0100

    role minecraft: Override java version

--- modules/archessmn/roles/minecraft/_default.nix
+++ modules/archessmn/roles/minecraft/_default.nix
@@ -33,7 +33,11 @@ in
           archessmn = "a2bb6157-d5e7-4d2a-9a6e-e534b0b17235";
           Empleon = "7362abdc-61bb-465a-8c4e-9744520d5bb4";
         };
-        package = pkgs.fabricServers.fabric-26_2;
+        package = pkgs.fabricServers.fabric-26_2.override (old: {
+          jre_headless = lib.warnIf (
+            lib.versions.major old.jre_headless.version == "25"
+          ) "nix-minecraft is using Java 25, override is now redundant" pkgs.openjdk25_headless;
+        });
       };
     };
 
